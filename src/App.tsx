6de2deaa7067610841847ab1c0b9f11145b4c4d3
Matteo Giannone <matteogiannone24@gmail.com>
2023-05-26 14:07:01 -0400

    Improved responsiveness and added buttons

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,18 @@ import SudokuBoard from './Components/SudokuBoard';
 function App() {
   return (
     <>
-        <h1>Sudoku Solver</h1>
+        <div className='content-div bg-lightbeige'>
+            <h1>Sudoku Solver</h1>
+        </div>
+        <div className='content-div'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis quae blanditiis velit fugiat. Quibusdam natus sunt minima id rerum excepturi repellendus vel perspiciatis culpa? Magni praesentium neque saepe perferendis tenetur!</p>
+        </div>
         <div className='bg-wood board-div'>
             <SudokuBoard />
+        </div>
+        <div className='button-div'>
+            <button className='btn-solve'>Solve</button>
+            <button className='btn-clear'>Clear</button>
         </div>
     </>
   );
