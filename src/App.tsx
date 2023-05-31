7e713cf73508b2ce2a0706e16b5da33e31bd26c7
Matteo Giannone <matteogiannone24@gmail.com>
2023-05-31 11:47:36 -0400

    Added messages displaying status of solver

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,53 @@ export enum BoardStatus {
     Fail = "hsl(0, 100%, 90%)",
 };
 
+enum SolverStatus {
+    NotStarted,
+    Working,
+    Succeeded,
+    Failed
+}
+
 function App() {
     let [board, updateBoard] = useState(new Array(9).fill(new Array(9).fill('')));
     let [boardStatus, updateBoardStatus] = useState(BoardStatus.Neutral);
+    let [solverStatus, updateSolverStatus] = useState(SolverStatus.NotStarted);
 
     function clearBoard() {
         updateBoard(new Array(9).fill(new Array(9).fill('')));
         updateBoardStatus(BoardStatus.Neutral);
+        updateSolverStatus(SolverStatus.NotStarted);
     }
 
     function solveBoard() {
         let sudokuSolver = new Worker(new URL("./Helpers/solver.ts", import.meta.url));
+        console.log(board)
         sudokuSolver.postMessage(board);
+        updateSolverStatus(SolverStatus.Working);
         
         sudokuSolver.onmessage = (e: MessageEvent) => {
             if(e.data !== null) {
                 updateBoard(e.data);
                 updateBoardStatus(BoardStatus.Success);
+                updateSolverStatus(SolverStatus.Succeeded);
             } 
             else {
                 updateBoardStatus(BoardStatus.Fail);
+                updateSolverStatus(SolverStatus.Failed);
             }
+        }
+    }
+
+    function getSolverStatusMessage() {
+        switch(solverStatus) {
+            case SolverStatus.NotStarted:
+                return "Fill out the Sudoku board and let the solver complete it!";
+            case SolverStatus.Working:
+                return "Solving the current Sudoku board. This may take some time.";
+            case SolverStatus.Succeeded:
+                return "A solution has been found!";
+            case SolverStatus.Failed:
+                return "There is no possible solution.";
         }
     }
 
@@ -47,6 +73,9 @@ function App() {
                     <button className='btn-solve' onClick={solveBoard}>Solve</button>
                     <button className='btn-clear' onClick={clearBoard}>Clear</button>
                 </div>
+            </div>
+            <div className='content-div'>
+                <p>{getSolverStatusMessage()}</p>
             </div>
         </>
     );
